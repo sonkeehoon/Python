479b2cd53b2b577c0ba46b54f470f3912e968e00
kee_hoon <djfkfk12345@naver.com>
2022-04-14 19:04:42 +0900

    220414 rjust함수

--- note.py
+++ note.py
@@ -10,13 +10,21 @@
 # print(isum)
 
 # 2차원 리스트를 초기화 할때 꼭 이런방식으로 하자
-k,n=3,3
-res1=[]
-for i in range(k):
-    line=[]
-    for j in range(n):
-        line.append(0)
-    res1.append(line)
-print(res1)
+# k,n=3,3
+# res1=[]
+# for i in range(k):
+#     line=[]
+#     for j in range(n):
+#         line.append(0)
+#     res1.append(line)
+# print(res1)
 
+import random
+z=0
+random.seed(random.random())
+def 난수(x,y):
+    z=random.random()
+    return z
+x,y=map(int,input().split())
+print(난수(x,y))
         
