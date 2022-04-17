b5e34f0bebc7465cfaabe0774dc5929069fcb890
kee_hoon <djfkfk12345@naver.com>
2022-04-17 23:58:23 +0900

    소수 구할때는 항상 제곱근으로!

---
+++ B1929.py
@@ -0,0 +1,17 @@
+import time
+def Prime(N):
+    last=int(N**0.5) # 제곱근만 검사해도 소수인지 판별 가능
+    for i in range(2,last+1):
+        if N%i==0:
+            return False
+    return True
+M,N=map(int,input().split())
+start=time.time()
+lst=[]
+for i in range(M,N+1):
+    if i==1:continue
+    if Prime(i):
+        lst.append(i)
+for j in lst:
+    print(j)
+print("exe time is : {:.3f}".format(time.time()-start))
