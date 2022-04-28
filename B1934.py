c8bcb0c9dd23ebcc6813957cbb0276d73e195b5a
kee_hoon <djfkfk12345@naver.com>
2022-04-28 21:51:54 +0900

    실버5 최소공배수 속도개선

--- B1934.py
+++ B1934.py
@@ -1,9 +1,7 @@
 import sys
-T=int(sys.stdin.readline())
+import math
+input=sys.stdin.readline
+T=int(input())
 for _ in range(T):
-    x,y=map(int,sys.stdin.readline().split())
-    lst=[]
-    for i in range(1,min(x,y)+1):
-        if x%i==0 and y%i==0:
-            lst.append(i)
-    print(x*y//max(lst))
+    x,y=map(int,input().split())
+    print(x*y//math.gcd(x,y))
