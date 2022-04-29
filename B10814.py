12821639381b65aaacb7d24b634bd016b7c912f6
kee_hoon <djfkfk12345@naver.com>
2022-04-30 01:07:20 +0900

    실버5 나이순정렬 배울것이 많은 문제였다

--- B10814.py
+++ B10814.py
@@ -1,7 +1,7 @@
 import sys
 N=int(sys.stdin.readline())
-lst=[sys.stdin.readline().split() for i in range(N)]
-lst=[(int(i[0]),i[1]) for i in lst]
-lst=sorted(lst,key=lambda x: (x[0]))
+lst=[sys.stdin.readline().split() for i in range(N)] # 2차원 리스트에 저장
+lst=[(int(i[0]),i[1]) for i in lst] # 나이값들만 정수로 변환
+lst=sorted(lst,key=lambda x: (x[0])) # 리스트 각요소의 0번째 요소를 ket(기준)으로 정렬
 for i in lst:
     print(i[0],i[1])
