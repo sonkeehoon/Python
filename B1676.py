cd7104b0ea76fe1e18baccd6a0cc04028a21d574
kee_hoon <djfkfk12345@naver.com>
2022-04-30 23:16:51 +0900

    실버4 팩토리얼0의개수 220430

--- B1676.py
+++ B1676.py
@@ -1,6 +1,6 @@
 from math import factorial as fact
 N=int(input())
-fact_lst=[int(i) for i in list(str(fact(N)))]
+fact_lst=list(map(lambda x: int(x),list(str(fact(N)))))
 fact_len=len(str(fact(N)))
 cnt=0
 for i in range(fact_len-1,-1,-1):
