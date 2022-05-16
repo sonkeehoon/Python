a499e8706b3eea41154c463c12657e7e97ac04e3
kee_hoon <djfkfk12345@naver.com>
2022-05-17 02:45:56 +0900

    실버4 점점 백준 슬럼프가 온다

---
+++ B1920.py
@@ -0,0 +1,12 @@
+import sys
+input=sys.stdin.readline
+N=int(input())
+A=list(map(int,input().split()))
+M=int(input())
+B=list(map(int,input().split()))
+A=set(A)
+for i in B:
+    if i in A:
+        print(1)
+        continue
+    print(0)
