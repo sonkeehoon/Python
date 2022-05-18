37868cfbb6023b27a2c0e11ecb85742b43f140c2
kee_hoon <djfkfk12345@naver.com>
2022-05-19 02:48:33 +0900

    참외밭

--- 2/B2477.py
+++ 2/B2477.py
@@ -1,14 +1,23 @@
+# 장시간 끝에 해결했는데 실버4였다니..
 import sys
 input=sys.stdin.readline
 K=int(input())
-d={}
+d={1:[],2:[],3:[],4:[]}
 lst=[]
 for i in range(6):
     x,y=map(int,input().split())
+    d[x].append(y)
     lst.append(y)
-    d[y]=x
 res=lst+lst
-for i in range(6):
-    if d[res[i]]==d[res[i+2]]:
-        print((res[i+4]*res[i+5]-(res[i+1]*res[i+2]))*K)
+max_len,min_len=[],[]
+for i in range(1,5):
+    if len(d[i])==1:
+        max_len.append(*d[i])
+for j in range(1,7):
+    if res[j]==max_len[0]:
+        min_len.append(abs(res[j-1]-res[j+1]))
         break
+for j in range(1,7):
+    if res[j]==max_len[1]:
+        min_len.append(abs(res[j-1]-res[j+1]))
+print((max_len[0]*max_len[1]-min_len[0]*min_len[1])*K)
