c2f54df9f0614782630392cbfea9a4d6b4131786
kee_hoon <djfkfk12345@naver.com>
2022-05-22 00:57:59 +0900

    하키

--- B1358.py
+++ B1358.py
@@ -1,8 +1,14 @@
 import sys
 input=sys.stdin.readline
 W,H,X,Y,P=map(int,input().split())
-lst=[]
+cnt=0
+R=H/2
 for i in range(P):
     x,y=map(int,input().split())
-    lst.append([x,y])
-    
+    if X<=x<=X+W and Y<=y<=Y+H:
+        cnt+=1
+        continue
+    elif (x-X)**2+(y-(Y+R))**2<=R**2 or (x-(X+W))**2+(y-(Y+R))**2<=R**2:
+        cnt+=1 
+    continue
+print(cnt)
