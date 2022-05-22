7107d3b4b3006f9039cbf2180d283a6dcb7e6a5d
kee_hoon <djfkfk12345@naver.com>
2022-05-22 17:12:04 +0900

    n

--- note.py
+++ note.py
@@ -23,4 +23,5 @@ from itertools import permutations as perm
 N,M=map(int,input().split())
 lst=[str(i) for i in range(1,N+1)]
 for i in perm(lst,M):
-    print(' '.join(i))    
+    print(' '.join(i))
+
