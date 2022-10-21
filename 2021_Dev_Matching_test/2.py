383b000611770a5b406bf958681a5c4bf3e8e43e
kee_hoon <djfkfk12345@naver.com>
2022-10-22 02:12:15 +0900

    221022

--- 2021_Dev_Matching_test/2.py
+++ 2021_Dev_Matching_test/2.py
@@ -10,9 +10,9 @@ def solution(rows, columns, queries):
         y1=query[1]-1
         x2=query[2]-1
         y2=query[3]-1
-        for i in range(y1,y2+1):
-            if i==0 or i==
-            for j in range(x1,x2+1):
+        # for i in range(y1,y2+1):
+        #     if i==0 or i==
+        #     for j in range(x1,x2+1):
         
         
         
