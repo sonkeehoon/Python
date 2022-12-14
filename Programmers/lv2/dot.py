0352ee18f7767bf58d6ea3862e949313a406d402
kh <81700507+sonkeehoon@users.noreply.github.com>
2022-12-14 22:33:40 +0900

    Update dot.py

--- Programmers/lv2/dot.py
+++ Programmers/lv2/dot.py
@@ -1,4 +1,6 @@
-# 68.8
+# 점 찍기 : https://school.programmers.co.kr/learn/courses/30/lessons/140107
+# 쉬울줄 알았는데 생각보다 어려웠다.. 처음엔 68.8을 받았고
+# 다른사람의 코드를 참고했다..
 import math
 def solution(k, d):
     answer = 0
