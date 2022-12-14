9024def7c7ffc8f32a74d759e6fbb9f45f71f955
kee_hoon <djfkfk12345@naver.com>
2022-12-15 00:30:11 +0900

    221215

--- Programmers/lv2/dot.py
+++ Programmers/lv2/dot.py
@@ -4,15 +4,10 @@
 import math
 def solution(k, d):
     answer = 0
-    
     n = d//k
-    
     for i in range(n+1):
         answer += int(math.sqrt(d**2-(i*k)**2))//k + 1
     
-    
-    
-      
     return answer
 
 print(solution(2,4))
