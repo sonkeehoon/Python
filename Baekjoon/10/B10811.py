1e7ae3401ffcdaceb0f2d970ccd994413d307218
kee_hoon <djfkfk12345@naver.com>
2023-03-04 13:35:46 +0900

    내용수정

--- Baekjoon/10/B10811.py
+++ Baekjoon/10/B10811.py
@@ -6,6 +6,6 @@ basket = [i+1 for i in range(N)]
 for _ in range(M):
     i,j = map(int,input().split())
     # basket[i-1:j] = reversed(basket[i-1:j]) 
-    basket[i-1:j] = basket[i-1:j][::-1] # reversed보다 [::-1] 이 속도가 약간더 빠름
+    basket[i-1:j] = basket[i-1:j][::-1] # reversed보다 약간더 빠른 방법
     
 print(*basket)
