9778a1aa07994f04912bdf09a60b1b6b49330b4f
keehoon <djfkfk12345@naver.com>
2023-03-06 16:57:48 +0900

    230306

--- Baekjoon/1/B1874.py
+++ Baekjoon/1/B1874.py
@@ -1,49 +1,10 @@
 # (틀린문제)나중에 재시도 해보자
+# 스택에 push하는 순서는 반드시 오름차순
+# push : +, pop : -
 import sys
-from collections import deque
 input = sys.stdin.readline
-n = int(input().strip())
-target,res,tmp = [],[],[]
-seq = deque(i for i in range(1, n+1))
-
-for _ in range(n):
-    target.append(int(input().strip()))
-target = list(reversed(target))
-lst = []
-x = 0
-for idx,t in enumerate(target):
-    if len(seq) == 0:
-        if tmp[-1] != t:
-            seq = deque(reversed(tmp))
-            tmp = []
-            while lst:
-                seq.appendleft(lst.pop())
-                res.append('-')
-            break
-        tmp.append(lst.pop())
-        res.append('-')
-        
-    else:
-        if seq[0] == t:
-            lst.append(seq.popleft())
-            res.append('+')
-            continue
-        elif seq[0] < t:
-            while seq[0] != t:
-                lst.append(seq.popleft())
-                res.append('+')
-            while target[idx-1] != lst[-1]:
-                tmp.append(lst.pop())
-                res.append('-')
-            lst.append(seq.popleft())
-            res.append('+')
-        else:
-            print("NO")
-            break
-print(seq,lst,tmp,res)
-if list(seq) == target:
-    for r in res:
-        print(r)
-    
-
+n = int(input())
+stack = [i+1 for i in range(n)]
+for _ in range(n): # 1이상 n이하의 정수가 하나씩 순서대로
+    pass
     
