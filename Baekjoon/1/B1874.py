c5e9e3ecf208106dca075380238f704cc9c34e4f
kee_hoon <djfkfk12345@naver.com>
2023-03-06 22:02:33 +0900

    230306

--- Baekjoon/1/B1874.py
+++ Baekjoon/1/B1874.py
@@ -1,15 +1,9 @@
-# (틀린문제)나중에 재시도 해보자(실버2)
-# 재시도 후 통과했다!
-# 이해가 안될때는 연습장에 쓰면서 해보는것도 좋은방법 같다.
-# 스택에 push하는 순서는 반드시 오름차순
-# push : +, pop : -
 import sys
 input = sys.stdin.readline
 n = int(input())
 tmp = [i for i in range(n,0,-1)]
 target = []
 seq, seq2, res = [], [], []
-avail=True
 for _ in range(n): # 1이상 n이하의 정수가 하나씩 순서대로
     target.append(int(input()))
 i = 0
@@ -22,13 +16,7 @@ while tmp or seq:
         res.append('-')
     if len(seq) > 0 and target[i] < seq[-1]:
         print("NO")
-        avail=False
-        break
+        exit()
     i += 1
-if avail:
-    for r in res:
-        print(r)
-    
-        
-    
-    
+for r in res:
+    print(r)
