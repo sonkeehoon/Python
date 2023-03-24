d92f500303329d3aa762c1471465abdad8d74fa0
kee_hoon <djfkfk12345@naver.com>
2023-03-24 22:08:03 +0900

    230324

--- Baekjoon/24/B24479.py
+++ Baekjoon/24/B24479.py
@@ -1,56 +1,38 @@
+# 90%에서 시간 초과가 뜬다
+# 남의 코드를 그대로 제출하긴 싫어서 이대로 두려고 한다
 import sys
 from collections import defaultdict,deque
 input = sys.stdin.readline
-import time
 
 N,M,R = map(int,input().split())
 # 정점의 수 N, 간선의 수 M, 시작 정점 R
+
 graph = defaultdict(list)
-# print(graph)
 for _ in range(1,M+1):
     u,v = map(int,input().split())
-    # u와 v는 연결
+    # u와 v 연결
     graph[u].append(v)
+    graph[v].append(u)
 # 각 key의 value 들을 오름차순 정렬
-for k in graph.keys():
+for k in graph:
     graph[k].sort()
-    graph[k] = deque(graph[k])
-order = defaultdict(int)
-order[R] = 1
+visited = [0]*(N+1)
+visited[R] = 1
 queue = deque([R])
 cnt = 2
-print(graph)
-# graph = {1: [2, 4], 2: [3, 4], 3: [4]}
+# graph = {1: [2,4], 2: [1,3,4], 3: [2,4], 4: [1,2,3]}
 while queue:
-    tmp = queue[-1] # 시작 노드 지정
-    print(queue,graph, tmp, order)
-    if not graph[tmp]:
-        queue.pop()
-        continue
+    tmp = queue.pop()
     
-    for i in graph[tmp]:
-        if i in order.keys(): # 이미 방문 했다면
+    for g in graph[tmp]:
+        
+        if visited[g] > 0: # 다음 노드가 방문 한거면 pass
             continue
-        order[i] = cnt
+        
+        queue.append(tmp) 
+        visited[g] = cnt
         cnt += 1
-        queue.append(i)
-        
+        queue.append(g)
         break
-    time.sleep(2)
-print(order,cnt)
-for i in range(1,N+1):
-    print(order[i])
-        
-        
-        
     
-        
-    
-    
-    
-    
-
-        
-    
-    
-
+print(*visited[1:], sep='\n')
