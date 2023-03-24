e3da1a506fff9e165a47037bb4d356f00da95f0c
kee_hoon <djfkfk12345@naver.com>
2023-03-24 22:08:26 +0900

    230324

--- Baekjoon/1/B1735.py
+++ Baekjoon/1/B1735.py
@@ -9,8 +9,10 @@ def divisor(res, lst : list): # 약수를 구해주는 함수
 x1,y1 = map(int,input().split())
 x2,y2 = map(int,input().split())
 res1,res2 = x1*y2 + x2*y1, y1*y2 
+
 # 두 수가 서로소가 될때까지 나눔
 lst1,lst2=[],[]
 lst1,lst2 = divisor(res1, lst1), divisor(res2,lst2)
 t = max(set(lst1) & set(lst2))
+
 print(res1//t,res2//t)
