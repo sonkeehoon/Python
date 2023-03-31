890ae2e02a91689b22f510d90bdbc943f6fb0239
kee_hoon <djfkfk12345@naver.com>
2023-03-31 15:26:34 +0900

    230331

--- Programmers/sql/String_Date/car_rental.sql
+++ Programmers/sql/String_Date/car_rental.sql
@@ -1,3 +1,4 @@
+-- 자동차 대여 기록에서 장기/단기 대여 구분하기 : https://school.programmers.co.kr/learn/courses/30/lessons/151138
 SELECT history_id
 , car_id, date_format(start_date, '%Y-%m-%d') as start_date
 , date_format(end_date, "%Y-%m-%d") as end_date,
