6a569077a174e8f802cab8fb7e3cd770437178de
kee_hoon <djfkfk12345@naver.com>
2023-04-01 12:20:24 +0900

    230401

--- Baekjoon/7/B7785.py
+++ Baekjoon/7/B7785.py
@@ -11,3 +11,8 @@ for _ in range(n):
         del d[name]
 print(*sorted(d.keys(), reverse=True), sep='\n')
 
+# 딕셔너리 ,집합 여러가지 방식으로 풀어봤는데 
+# 메모리와 시간을 가장 적게(효율적으로)쓰려면 딕셔너리가 정답이었다
+# enter일때 이름을 딕셔너리에 넣고 leave면 빼는 방식
+# 깨달은점 : set에 sorted를 쓰면 정렬해서 list로 반환(집합에 sort연산은 불가능!)
+
