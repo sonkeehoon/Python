2f2c616bba51e8947762577aed9aecc388f351b1
kee_hoon <djfkfk12345@naver.com>
2023-04-01 14:26:22 +0900

    230401

--- Baekjoon/13/B13909.py
+++ Baekjoon/13/B13909.py
@@ -4,3 +4,4 @@ print(int(int(input())**0.5))
 # 공약수, 공배수 : a common divisor, a common multiple
 # 최대공약수 : GCD(the greatest common denominator,factor)
 # 최대공약수 : LCM(the least common multiple)
+# del이 리스트에서는 O(n)인데 딕셔너리 에서는 O(1)의 시간복잡도를 갖는다
