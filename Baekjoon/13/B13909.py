9abac263c58b654253ffabddd7a2ddb4622c686b
kee_hoon <djfkfk12345@naver.com>
2023-04-01 13:10:21 +0900

    230401

---
+++ Baekjoon/13/B13909.py
@@ -0,0 +1,6 @@
+print(int(int(input())**0.5))
+# 배수 : multiple
+# 약수 : divisor
+# 공약수, 공배수 : a common divisor, a common multiple
+# 최대공약수 : GCD(the greatest common denominator,factor)
+# 최대공약수 : LCM(the least common multiple)
