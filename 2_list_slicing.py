7de09a85016b581e674cb3831ff7b965bde1818c
kee_hoon <djfkfk12345@naver.com>
2023-04-09 00:35:10 +0900

    230409

---
+++ 2_list_slicing.py
@@ -0,0 +1,12 @@
+two_lst = [[i*5+j for j in range(1,6)] for i in range(5)]
+print(*two_lst,sep='\n')
+# 가운데 [7,8,9],[12,13,14],[17,18,19] 를 추출해보자
+# (1,1) 부터 (3,3) 까지
+for i in range(1,4):
+    print("[",end='')
+    for j in range(1,4):
+        if j < 3:
+            print(two_lst[i][j],end=', ')
+        else:
+            print(two_lst[i][j],end='')
+    print("]")
