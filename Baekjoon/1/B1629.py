df5472f0b26c9b49a272b0c9fc2d0b15a381d09e
kee_hoon <djfkfk12345@naver.com>
2023-04-17 19:44:11 +0900

    230417

--- Baekjoon/1/B1629.py
+++ Baekjoon/1/B1629.py
@@ -1,4 +1,24 @@
+# 곱셈 : https://www.acmicpc.net/problem/1629
+# 스스로 해결 여부 : O (pow 함수로만)
+# pow 함수로 답만내고 넘어갔던 문제
 import sys
 input=sys.stdin.readline
 A,B,C=map(int,input().split())
-print(pow(A,B,C))
+# print(pow(A,B,C)) 꼼수(?)로 썼던 함수
+
+# 예전에 꼼수로 풀고 넘어갔었다. 원래는 분할정복으로 풀어야한다
+# B11444번 문제에서 분할정복을 마주쳤는데 기억이 안나서 이 문제(B1629.py)를 먼저 풀어보려고 한다
+# https://www.acmicpc.net/problem/11444
+
+def multi(a,b): # 재귀함수
+    
+    if b == 1:
+        return a%C
+    else:
+        tmp = multi(a,b//2) # 쉽게 생각하면 반으로 계~속 쪼개고 홀수인지 짝수인지에 따라 분기를 나눠준다
+        if b % 2 == 0: # 짝수면
+            return (tmp**2)%C
+        else:
+            return ((tmp**2)*a)%C
+
+print(multi(A,B))
--- Baekjoon/11/B11444.py
+++ Baekjoon/11/B11444.py
@@ -1,12 +1,44 @@
 # 피보나치 수 6 : https://www.acmicpc.net/problem/11444
 # 스스로 해결 여부 : X
+# 이 문제를 풀기 위해 B1629.py를 다시 풀었다
+# 분할정복과 이차원행렬 곱셈을 알아야 풀수 있는 문제다
+# 문제가 어려워서 남의 코드를 붙여넣기 하고 종이에 써가면서 로직만 이해했다
+# 참고 : https://cantcoding.tistory.com/28
+
+# 제곱
+def power(f,n):
+    if n == 1:
+        return f
+    elif n % 2 == 0: # 짝수면
+        return power(multi(f,f),n//2)
+    else: # 홀수면
+        return multi(power(f,n-1),f)
+    
+def multi(a,b): # 2차원 행렬 a와 b를 곱해주는 함수
+    tmp = [[0]*len(b[0]) for _ in range(2)]
+    for i in range(2):
+        for j in range(len(b[0])):
+            sum = 0
+            for k in range(2):
+                sum += a[i][k] * b[k][j]
+            tmp[i][j] = sum % div # 미리나눠서 tmp에 더해줌
+    return tmp
 
 n = int(input())
-# divisor = 1000000007
-divisor = 41
+f = [[1,1],[1,0]]
+start = [[1],[1]]
+div = 1000000007
 
-F = [0,1] + [0]*(n-1)
-for i in range(2, len(F)):
-    F[i] = (F[i-2] + F[i-1])%divisor
-print(F[:n])
+if n < 3: 
+    print(1)
+else: # n >= 3
+    print(multi(power(f, n-2), start)[0][0])
+    
+    
+
+
+        
+        
+
+
         
