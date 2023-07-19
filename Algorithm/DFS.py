b1bec243b29c980631262008bbed1269cde5516b
kee_hoon <djfkfk12345@naver.com>
2023-07-19 18:11:40 +0900

    230719

---
+++ Algorithm/DFS.py
@@ -0,0 +1,32 @@
+from graph import graph
+from collections import deque
+
+def dfs(graph, start_node):
+     
+    ## 기본은 항상 두개의 리스트를 별도로 관리해주는 것
+    need_visited, visited = deque(), list()
+ 
+    ## 시작 노드를 시정하기 
+    need_visited.append(start_node)
+    
+    ## 만약 아직도 방문이 필요한 노드가 있다면,
+    while need_visited:
+ 
+        ## 그 중에서 가장 마지막 데이터를 추출 (스택 구조의 활용)
+        node = need_visited.pop()
+        
+        ## 만약 그 노드가 방문한 목록에 없다면
+        if node not in visited:
+ 
+            ## 방문한 목록에 추가하기 
+            visited.append(node)
+ 
+            ## 그 노드에 연결된 노드를 
+            need_visited.extend(graph[node])
+            # need_visited += graph[node]
+            
+            
+    return visited
+
+tmp = dfs(graph, 'A')
+print(*tmp,sep=', ')
