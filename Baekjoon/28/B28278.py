26396784d4660e2e891c8a469a93bd74793fcefc
kee_hoon <djfkfk12345@naver.com>
2023-10-20 19:27:04 +0900

    231020

---
+++ Baekjoon/28/B28278.py
@@ -0,0 +1,33 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+N = int(input())
+
+stack = deque()
+for _ in range(N):
+    cmd = input()
+    if cmd[0] == "1":
+        X = int(cmd[2:])
+        stack.append(X)
+        
+    elif cmd[0] == "2":
+        if stack:
+            print(stack.pop())
+            continue
+        print(-1)
+    
+    elif cmd[0] == "3":
+        size = len(stack)
+        print(size)
+    
+    elif cmd[0] == "4":
+        if stack:
+            print(0)
+            continue
+        print(1)
+    
+    elif cmd[0] == "5":
+        if stack:
+            print(stack[-1])
+            continue
+        print(-1)
