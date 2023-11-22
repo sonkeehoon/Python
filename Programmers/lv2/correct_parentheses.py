ffcaba53ebcc37ec11d7f254579b011985cd7a86
kee_hoon <djfkfk12345@naver.com>
2023-11-22 21:44:31 +0900

    231122

--- Programmers/lv2/correct_parentheses.py
+++ Programmers/lv2/correct_parentheses.py
@@ -1,15 +1,20 @@
 # 올바른 괄호 : https://school.programmers.co.kr/learn/courses/30/lessons/12909
 def solution(s):
-    l = len(s)
-    if l % 2 == 1 or s[-1] == '(' or s[0] == ')' or len(s) != 2*s.count('(') :
+    from collections import deque
+    s = deque(s)
+    q = deque()
+    while s:
+        tmp = s.pop()
+        q.appendleft(tmp)
+        try:
+            if q[0] == "(" and q[1] == ")":
+                q.popleft()
+                q.popleft()                
+        except IndexError:
+            break              
+    if q:
         return False
-    
-    for i in range(1,l):
-        if s[i-1] == '(' and s[i] == ')' :
-            s = 'x'*2 + s[:i-1] + s[i+1:]
-    if s == 'x'*l:
-        return True
-    return False
+    return True
 
 print(solution("()()"))
 print(solution("(())()"))
