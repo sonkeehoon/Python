5d91582a23fe6750477676325ce32deba200b807
kee_hoon <djfkfk12345@naver.com>
2023-12-14 23:39:17 +0900

    231214

---
+++ Baekjoon/14/B14888.py
@@ -0,0 +1,39 @@
+# 연산자 끠워넣기 : https://www.acmicpc.net/problem/14888
+import sys
+from itertools import permutations as perm
+input = sys.stdin.readline
+
+N = int(input())
+A = list(map(int, input().split())) # 숫자
+operator_cnt = list(map(int, input().split())) # 연산 (+, -, *, //)
+operator = ['+','-','*','//']
+operator2 = []
+for i in range(4):
+    for _ in range(operator_cnt[i]):
+        operator2.append(operator[i])
+        
+def calc(x, y, case):
+    if case == '+': return x + y
+    elif case == '-': return x - y
+    elif case == '*': return x * y
+    elif case == '//': 
+        if x < 0:
+            return - (abs(x) // y)
+        else:
+            return x//y
+
+minnum = int(1e10)
+maxnum = -int(1e10)
+cases = set(perm(operator2, N-1)) # list로 하면 중복인 케이스 까지 전부 넣어서 시간초과
+
+for case in cases:
+    total = calc(A[0], A[1], case[0])
+    
+    for i in range(1, N-1):
+        total = calc(total, A[i+1], case[i])
+        
+    maxnum = max(maxnum, total)
+    minnum = min(minnum, total)
+
+print(maxnum)
+print(minnum)
