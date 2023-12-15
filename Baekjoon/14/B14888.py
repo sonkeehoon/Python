4b6fca158af77732c515152731192836054302ca
kee_hoon <djfkfk12345@naver.com>
2023-12-15 21:19:31 +0900

    231215

--- Baekjoon/14/B14888.py
+++ Baekjoon/14/B14888.py
@@ -1,13 +1,14 @@
 # 연산자 끠워넣기 : https://www.acmicpc.net/problem/14888
 import sys
 from itertools import permutations as perm
-input = sys.stdin.readline
 
+input = sys.stdin.readline
 N = int(input())
 A = list(map(int, input().split())) # 숫자
 operator_cnt = list(map(int, input().split())) # 연산 (+, -, *, //)
 operator = ['+','-','*','//']
 operator2 = []
+
 for i in range(4):
     for _ in range(operator_cnt[i]):
         operator2.append(operator[i])
@@ -24,7 +25,7 @@ def calc(x, y, case):
 
 minnum = int(1e10)
 maxnum = -int(1e10)
-cases = set(perm(operator2, N-1)) # list로 하면 중복인 케이스 까지 전부 넣어서 시간초과
+cases = set(perm(operator2, N-1)) # set이 아닌 list로 하면 중복인 케이스까지 넣어서 시간초과
 
 for case in cases:
     total = calc(A[0], A[1], case[0])
