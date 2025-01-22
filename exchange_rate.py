08b86e5814eaeee509bcddbf975cdf9e6f3cd588
kee_hoon <djfkfk12345@naver.com>
2025-01-22 20:24:55 +0900

    exchange_rate

--- exchange_rate.py
+++ exchange_rate.py
@@ -2,6 +2,8 @@
 import requests
 from bs4 import BeautifulSoup
 import datetime
+from pathlib import Path
+
 
 # 네이버의 환율페이지를 참고
 url = "https://finance.naver.com/marketindex/exchangeList.naver"
@@ -12,13 +14,14 @@ if res.status_code == 200: # 정상 연결상태면
     soup = BeautifulSoup(txt, 'html.parser')
     td_titles = soup.find_all('td', attrs={"class":"tit"})
     td_sales = soup.find_all('td', attrs={"class":"sale"})
-    f = open("exchange_rate.txt", 'w', encoding="utf-8")
+    path = Path.home()/"Desktop"
+    f = open(f"{path}/실시간_전세계_환율.txt", 'w', encoding="utf-8")
     f.write(str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M'))+ ' 환율\n\n')
     for i in range(len(td_titles)):
         title = td_titles[i]
         sale = td_sales[i]
-        
-        f.write(f'{title.a.text.strip():-<20}{sale.text}\n')
+        print(f"{title.a.text.strip():-<20} {sale.text}")
+        f.write(f"{title.a.text.strip():-<20} {sale.text}\n")
 
 else:
     print(res.status_code)
