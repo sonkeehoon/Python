1e8e8655cae5ad59231acccc6f64cba67282a894
kee_hoon <djfkfk12345@naver.com>
2025-03-09 14:03:46 +0900

    sublist

--- sublist.py
+++ sublist.py
@@ -1,8 +1,4 @@
-# 2차원 리스트에서 부분리스트를 slicing해주는 코드
-
-    
-# 가운데 [7,8,9],[12,13,14],[17,18,19] 를 추출해보자
-# (x1,y1) 부터 (x2,y2) 까지
+# 2차원 리스트에서 부분리스트만 추출해주는 코드
 
 def get_sublist(lst: list, 
                  x1: int, y1: int, 
@@ -11,14 +7,16 @@ def get_sublist(lst: list,
     sublist = []
     
     if x2 >= len(lst) or y2 >= len(lst[0]) or x1 < 0 or y1 < 0:
-        raise ValueError("잘못된 값입니다!")
-    if any(( type(coord) != int for coord in [x1, x2, y1, y2] )):
+        raise IndexError("범위를 벗어났습니다!")
+    
+    if any((type(coord) != int for coord in [x1, x2, y1, y2] )):
         raise TypeError("자연수만 입력하세요!")
         
     for i in range(x1, x2+1):
         line = []
         
         for j in range(y1, y2+1):
+            
             if j < x2:
                 line.append(lst[i][j])
                 
@@ -30,11 +28,10 @@ def get_sublist(lst: list,
     return sublist
 
 if __name__ == "__main__":
-    init_lst = [[i*5+j for j in range(1,6)] for i in range(5)]
-    print(*init_lst,sep='\n')
+    
+    init_lst = [[i*5 + j for j in range(1, 6)] for i in range(5)]
+    print(*init_lst, sep='\n')
     print()
     res = get_sublist(init_lst, 1, 1, 4, 3)
     print(res)
-    
-
     
