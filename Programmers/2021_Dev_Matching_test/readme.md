5778e801be0364aac4e06d214d11ffecf52acb12
kh <81700507+sonkeehoon@users.noreply.github.com>
2025-04-01 21:26:43 +0900

    Delete Programmers directory

--- Programmers/2021_Dev_Matching_test/readme.md
+++
@@ -1,2 +0,0 @@
-## 2021 Dev-Matching: 웹 백엔드 개발자(상반기) 문제
-https://school.programmers.co.kr/learn/challenges?page=1&languages=python3&partIds=21366
--- Programmers/lv2/next_num.py
+++
@@ -1,9 +0,0 @@
-# 다음 큰 숫자 : https://school.programmers.co.kr/learn/courses/30/lessons/12911
-def solution(n):
-    num_one = bin(n).count('1')
-    while True:
-        n += 1
-        if num_one == bin(n).count('1'):
-            return n
-
-print(solution(15))
