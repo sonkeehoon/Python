b45c2f338ca22089a2539ccda270f27c40a1dec6
kee_hoon <djfkfk12345@naver.com>
2025-06-02 22:57:33 +0900

    add B24479.py

--- Baekjoon/24/B24479.py
+++ Baekjoon/24/B24479.py
@@ -1,54 +1,36 @@
+# 알고리즘 수업 - 깊이 우선 탐색 1 : https://www.acmicpc.net/problem/24479
 import sys
-from collections import defaultdict,deque
-import heapq
 input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
 
-N, M, R = map(int,input().split())
-# 정점의 수 N, 간선의 수 M, 시작 정점 R
+N, M, R = map(int,input().split()) # 정점의 수 N, 간선의 수 M, 시작 정점 R
 
-graph = defaultdict(list)
-for _ in range(1,M+1):
-    u,v = map(int,input().split())
-    # u와 v 연결
+graph = [[] for _ in range(N + 1)]
+
+for _ in range(M):
+    u, v = map(int, input().split())
     graph[u].append(v)
     graph[v].append(u)
 
-for k in graph:
-    heapq.heapify(graph[k])
-visited = [0]*(N+1)
-visited[R] = 1
-# print(graph)
-queue = deque([R])
-cnt = 1
-# graph = {1: [2,4], 2: [1,3,4], 3: [2,4], 4: [1,2,3]}
-while queue:
-    tmp = queue.pop()
-    heap = graph[tmp]
-    if not heap:
-        continue
-    while heap:
-        next = heapq.heappop(heap)
-        if visited[next] == 0:
-            cnt += 1
-            visited[next] = cnt
-            break
-    # print(tmp, next, queue, cnt)
-    queue.append(tmp)
-    queue.append(next)
+for e in graph:
+    e.sort()
+
+visited = [False] * (N + 1)
+visited[R] = True
+res = [0] * (N + 1)
+order = 1
+res[R] = order
+
+def dfs(cur):
+    global order
     
-    
-        
-        
-    # for g in graph[tmp]:
-        
-    #     if visited[g] > 0: # 다음 노드가 방문 한거면 pass
-    #         continue
-        
-    #     queue.append(tmp) 
-    #     visited[g] = cnt
-    #     cnt += 1
-    #     queue.append(g)
-    #     break
-    
-for v in visited[1:]:
-    print(v)
+    for dest in graph[cur]:
+        if not visited[dest]:
+            order += 1
+            res[dest] = order
+            visited[dest] = True
+            dfs(dest)
+            
+dfs(R)
+
+print(*res[1:], sep = '\n')
